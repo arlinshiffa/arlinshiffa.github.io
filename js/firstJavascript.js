bc65897a001233f7977de548ca816ba8a995a70a
arlinshiffa <sasqia.shiffa@gmail.com>
2017-10-29 16:06:04 +0700

    update slider

---
+++ js/firstJavascript.js
@@ -0,0 +1,26 @@
+
+const a= 10;
+
+console.log(typeof a);
+
+ let b='something said,"hey you!"';
+ console.log(b);
+
+x=[];
+for(i=0;i<=100;i++){
+    x[i]=i;
+}
+
+function cat(color, age){
+    this.color=color;
+    this.age=age;
+    this.tambahUmur=function(){
+        this.age=this.age+5;
+    }
+}
+
+wawoosh= new cat("white", 10);
+wawoosh.tambahUmur();
+console.log(wawoosh.age);
+console.log(x.length);
+console.log(x);
